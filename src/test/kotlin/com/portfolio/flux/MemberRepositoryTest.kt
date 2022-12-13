bd26b8ff1f217d1b3f5cd98e7ad2262005e1a6a2
mlbase <guddnr2424@gmail.com>
2022-12-13 23:54:41 +0900

    kotest 코드 작성중

--- src/test/kotlin/com/portfolio/flux/MemberRepositoryTest.kt
+++ src/test/kotlin/com/portfolio/flux/MemberRepositoryTest.kt
@@ -4,25 +4,19 @@ import com.portfolio.flux.config.ServerConfig
 import com.portfolio.flux.model.Member
 import com.portfolio.flux.repository.MemberRepository
 import io.kotest.core.spec.style.FunSpec
-import io.kotest.core.spec.style.Test
-import io.kotest.matchers.should
-import io.kotest.matchers.shouldBe
+import io.kotest.extensions.spring.SpringExtension
 import io.kotest.matchers.shouldNotBe
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest
 import org.springframework.test.context.ContextConfiguration
-import org.springframework.test.context.junit.jupiter.SpringExtension
 
 
-//@WebFluxTest(FluxApplication::class)
-@ContextConfiguration(classes = [MemberRepository::class, ServerConfig::class, FluxApplication::class])
-class MemberRepositoryTest(memberRepository: MemberRepository) : FunSpec() {
+@ContextConfiguration(classes = [ServerConfig::class, MemberRepository::class])
+class MemberRepositoryTest(memberRepository: MemberRepository) : FunSpec({
 
-    init {
-        test("create test") {
-            var member: Member = Member(userName = "테스트", password = "1234", email = "test@example.ocm")
-            memberRepository.save(member)
-            member.id shouldNotBe null
-        }
+    extensions(SpringExtension)
+
+    test("create test") {
+        var member: Member = Member(userName = "테스트", password = "1234", email = "test@example.ocm")
+        val saved = memberRepository.save(member)
+        saved.id shouldNotBe null
     }
-}
+})
