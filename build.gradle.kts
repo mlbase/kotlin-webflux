871be59c79db72297e6ac9f19ccab81c9bb5147a
mlbase <guddnr2424@gmail.com>
2022-12-13 23:53:58 +0900

    spirngextension 관련 dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.14.RELEASE"
 	kotlin("jvm") version "1.6.21"
 	kotlin("plugin.spring") version "1.6.21"
-	id("io.kotest.multiplatform") version "5.0.3"
+	id("io.kotest.multiplatform") version "5.0.2"
 
 }
 
@@ -30,12 +30,14 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("io.kotest.extensions:kotest-extensions-spring:1.1.2")
 	runtimeOnly("mysql:mysql-connector-java")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("io.projectreactor:reactor-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
 	testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
 	testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
+
 }
 
 tasks.withType<KotlinCompile> {
